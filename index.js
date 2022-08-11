43ade09f0394104951c76891f5817563152d48cb
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-11 19:34:22 -0400

    wip

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ const PORT = process.env.PORT || 9000
 server.listen(PORT, () => {
     console.log("Listening on port: ", PORT)
 })
+
+server.use("/", (req, res) => {
+    res.send(`<h1>Hello World!</h1>`)
+})
